c23375358551076bbbb1b69b64396b5f75e775b4
ryancolinoconnor <42723691+ryancolinoconnor@users.noreply.github.com>
2024-04-14 16:33:47 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,4 +4,6 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_SYM, BSPC),U_LT(U_FUN, DEL),U_LT(U_NAV, TAB),U_LT(U_MEDIA, ESC),  U_LT(U_NUM,RET),U_LT(U_MOUSE, SPACE),   U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_SYM, BSPC), U_LT(U_FUN, DEL),  U_LT(U_NAV, TAB),  U_LT(U_MEDIA, ESC),U_LT(U_NUM,RET),U_LT(U_MOUSE, SPACE),   U_NP,              U_NP
+
+
