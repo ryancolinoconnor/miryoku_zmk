6c6d05e3ca823335da8745de78d3822b16e16c1b
ryancolinoconnor <ryan.oconnor1@live.com>
2026-01-19 13:36:07 -0500

    thing

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,3 +11,11 @@ U_NP,              U_NP,              U_LT(U_SYM, BSPC), U_LT(U_FUN, DEL),  U_LT
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_NAV, TAB),  U_LT(U_SYM, BSPC), U_LT(U_FUN, DEL),  U_LT(U_NUM,RET),U_LT(U_MOUSE, SPACE), U_LT(U_MEDIA, ESC),   U_NP,              U_NP
+
+#define MIRYOKU_LAYER_SYM \
+&kp AMPS,  &kp ASTR,  &kp LPAR,  &kp RPAR,  &kp CIRC,   &kp PIPE,  &kp EQUAL, &kp MINUS, &kp BSLH,  &kp GRAVE, \
+&kp DOLLAR,&kp PERCENT,&kp CIRC, &kp PLUS,  &kp AT,     &kp EXCL,  &kp DQT,   &kp APOS,  &kp COLON, &kp SEMI,  \
+&kp EXCL,  &kp AT,    &kp HASH,  &kp DOLLAR,&kp PERCENT,&kp LT,    &kp GT,    &kp COMMA, &kp DOT,   &kp SLASH,\
+U_NP,      U_NP,      U_NA,      &kp RPAR,  U_NA,       U_NA,      U_NA,      U_NA,      U_NP,      U_NP
+
+
